77f2fab27705e5d0a2fd9781174f9d4fd8d8a30e
RayHuang7164 <raymineg@gmail.com>
2023-04-08 13:36:43 +0800

    updeta

--- Lesson4_1.py
+++ Lesson4_1.py
@@ -1,8 +1,8 @@
 import private
 import requests
-url = f'https://maker.ifttt.com/trigger/button_press/with/key/{private.iftttkey}?value1=31c&value2=51'
+url = f'https://maker.ifttt.com/trigger/button_press/with/key/{private.iftttkey}?value1=32c&value2=54'
 #print(url)
 
 r = requests.get(url)
 if r.status_code == 200:
-    print("發送成功22")
+    print("發送成功")
