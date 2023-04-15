861de75da87f4112582894e4209d4aa94371e899
RayHuang7164 <raymineg@gmail.com>
2023-04-15 11:05:55 +0800

    update

--- Lesson5_1.py
+++ Lesson5_1.py
@@ -1,7 +1,7 @@
 import gpiozero
 # https://gpiozero.readthedocs.io/en/stable/api_spi.html#mcp3008
 from signal import pause  #執行完就停
-mcp3008 = gpiozero.mcp3008(channel=7)
+mcp3008 = gpiozero.MCP3008(channel=7)
 print(mcp3008.value)
 
 pause()
