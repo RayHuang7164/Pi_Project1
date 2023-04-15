9c33c54955c2816cde5ab93deb92f8c5a045b5e9
RayHuang7164 <raymineg@gmail.com>
2023-04-15 13:02:50 +0800

    update

---
+++ Lesson5_6.py
@@ -0,0 +1,20 @@
+   
+import gpiozero    #增加LM35溫度量測
+from time import sleep 
+# https://gpiozero.readthedocs.io/en/stable/api_spi.html#mcp3008
+
+mcp3008_light = gpiozero.MCP3008(channel=7)
+mcp3008_temperature = gpiozero.MCP3008(channel=6)
+buzzer = gpiozero.Buzzer(25) #設定25pin BZ
+
+while(True):    
+    lightValue = round(mcp3008_light.value*1000)
+    #temperature = (mcp3008_temperature.value*1000)
+    temperature = mcp3008_temperature.value* (1000)
+    print(temperature) 
+    #print(lightvalue)
+    if lightValue < 10:  #燈源不足的會觸發
+        buzzer.on()
+    else:
+        buzzer.off()
+    sleep(1)
