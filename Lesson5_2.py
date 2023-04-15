bfad5c000a3ebc7e654f4470b15ed7f03704fcba
RayHuang7164 <raymineg@gmail.com>
2023-04-15 11:09:20 +0800

    date

--- Lesson5_2.py
+++ Lesson5_2.py
@@ -1,7 +1,10 @@
 import gpiozero
 # https://gpiozero.readthedocs.io/en/stable/api_spi.html#mcp3008
 from signal import pause  #執行完就停
-mcp3008 = gpiozero.MCP3008(channel=7)
-print(mcp3008.value)
-
+from time import sleep  #執行完就停
+while(true):
+    mcp3008 = gpiozero.MCP3008(channel=7)
+    print(mcp3008.value)
+    sleep(1)
+    
 pause()
