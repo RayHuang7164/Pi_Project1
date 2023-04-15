131c61d4067f7b519ec1a4168ba7922d585fd114
RayHuang7164 <raymineg@gmail.com>
2023-04-15 11:09:49 +0800

    upera

--- Lesson5_2.py
+++ Lesson5_2.py
@@ -2,7 +2,7 @@ import gpiozero
 # https://gpiozero.readthedocs.io/en/stable/api_spi.html#mcp3008
 from signal import pause  #執行完就停
 from time import sleep  #執行完就停
-while(true):
+while(True):
     mcp3008 = gpiozero.MCP3008(channel=7)
     print(mcp3008.value)
     sleep(1)
