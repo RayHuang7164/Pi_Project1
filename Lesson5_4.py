ff7ff0d853a9bd7d5ddded11dbf8054736c42041
RayHuang7164 <raymineg@gmail.com>
2023-04-15 11:39:40 +0800

    updata

--- Lesson5_4.py
+++ Lesson5_4.py
@@ -2,7 +2,7 @@ import gpiozero
 # https://gpiozero.readthedocs.io/en/stable/api_spi.html#mcp3008
 from signal import pause  #執行完就停
 from time import sleep  #執行完就停
-from gpiozero import Buzzer  #增加BZ
+from gpiozero import Buzzer  #增加BZ 
 
 mcp3008 = gpiozero.MCP3008(channel=7)
 Buzzer = gpiozero.Buzzer(25) #設定25pin
@@ -10,7 +10,7 @@ Buzzer = gpiozero.Buzzer(25) #設定25pin
 while(True):
     lightvalue = round(mcp3008.value*1000)
     print(lightvalue)
-    if lightvalue < 40:
+    if lightvalue < 40:  #燈源不足的會觸發
         Buzzer.on()
     else:
         Buzzer.off()
