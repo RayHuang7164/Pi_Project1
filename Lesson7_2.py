874be05b6fb0abfc334babe9b586754699360f8c
RayHuang7164 <raymineg@gmail.com>
2023-04-22 14:34:22 +0800

    update

--- Lesson7_2.py
+++ Lesson7_2.py
@@ -8,7 +8,7 @@ from firebase_admin import db
 cred = credentials.Certificate("raspberry-1129a-firebase-adminsdk-75033-ae5ba56285.json")
 #firebase_admin.initialize_app(cred)
 
-
+# Database 位置 https://console.firebase.google.com/project/raspberry-1129a/database/raspberry-1129a-default-rtdb/data
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://raspberry-1129a-default-rtdb.asia-southeast1.firebasedatabase.app/'
